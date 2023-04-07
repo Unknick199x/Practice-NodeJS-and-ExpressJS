ca8a82e08ea4167d635c0a3490d2c7cee92e2d27
Unknick1996x <nickanthony.escobido@gmail.com>
2023-04-07 14:12:55 +0800

    learning #10

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("small amount");
+} else {
+  console.log("large number");
+}
+
+console.log("wow so nice !!");
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Practice-NodeJS-and-ExpressJS
 
-💻Learning how to use Node and
+💻Learning how to use Node.
+
+https://www.youtube.com/watch?v=Oe421EPjeBE&t=3871s
